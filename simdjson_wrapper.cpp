5e116c4de34f838677d4fab17e0d41f3cc1ca174
Juhász Péter <peter.juhasz83@gmail.com>
2023-03-25 18:56:00 +0100

    -Fix: hash keys are returned as utf-8, so store them into the perl hash as such

--- simdjson_wrapper.cpp
+++ simdjson_wrapper.cpp
@@ -140,7 +140,8 @@ static SV* recursive_parse_json(dec_t *dec, T element) {
         SV *sv_value = recursive_parse_json(dec, val);
         NULL_RETURN_CLEANUP(sv_value, hv);
 
-        hv_store (hv, key.data(), key.size(), sv_value, 0);
+        // perlapi: If klen is negative the key is assumed to be in UTF-8-encoded Unicode
+        hv_store (hv, key.data(), -key.size(), sv_value, 0);
       }
 
       DEC_DEC_DEPTH;
